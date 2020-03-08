e53464f3d3a18987e3ed822435f383e4105d7c5a
Alex Wey <53589584+alex-wey@users.noreply.github.com>
2020-03-08 12:52:38 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,11 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
 import sqlite3
-import urllib
-
-
-
-
 
 conn = sqlite3.connect(<database file>)
 c = conn.cursor()
@@ -15,14 +8,14 @@ c.execute('DROP TABLE IF EXISTS "Games";')
 c.execute('DROP TABLE IF EXISTS "Hands";')
 c.execute('DROP TABLE IF EXISTS "Actions";')
 
-#Create tables in the database and add data to it. REMEMBER TO COMMIT
-c.execute('''CREATE TABLE Games(game_ID varchar(256) not null, cards varchar(256), pre_flop_pot float, post_flop_pot float, post_turn_pot float, post_river_pot float)''')
+# Create tables in the database and add data to it. REMEMBER TO COMMIT
+c.execute('''CREATE TABLE Games(game_ID varchar(256) not null, stakes float, cards varchar(256), pre_flop_pot float, post_flop_pot float, post_turn_pot float, post_river_pot float)''')
 c.execute('''CREATE TABLE Hands(game_ID varchar(256) not null, player_ID varchar(256) not null, card_1 varchar(256), card_2 varchar(256), bets float, collects float, chips_at_begining float)''')
 c.execute('''CREATE TABLE Actions(game_ID varchar(256) not null, player_ID varchar(256) not null, round_k int not null, \
     position int not null, player_chips varchar(256), action_id varchar(256))''')
 conn.commit()
 
-#use to add stuff:
+# Insert values into respective tables
     c.execute('''INSERT INTO Games VALUES (?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
     c.execute('''INSERT INTO Hands VALUES (?, ?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
     c.execute('''INSERT INTO Actions VALUES (?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
