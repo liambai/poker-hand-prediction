ad5c07eea90d73859bb1a7c5552a19ad79dc7094
jlgood23 <jlgood23@icloud.com>
2020-03-08 16:49:49 -0400

    Update data.py

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ c.execute('DROP TABLE IF EXISTS "Actions";')
 
 # Create tables in the database and add data to it. REMEMBER TO COMMIT
 c.execute('''CREATE TABLE Games(game_ID varchar(256) not null, big_blind float, cards varchar(256), pre_flop_pot float, post_flop_pot float, post_turn_pot float, post_river_pot float)''')
-c.execute('''CREATE TABLE Hands(game_ID varchar(256) not null, player_ID varchar(256) not null, card_1 varchar(256), card_2 varchar(256), bets float, collects float, chips_at_begining float)''')
+c.execute('''CREATE TABLE Hands(game_ID varchar(256) not null, player_ID varchar(256) not null, card_1 varchar(256), card_2 varchar(256), bets float, net_gain float, chips_at_begining float)''')
 c.execute('''CREATE TABLE Actions(game_ID varchar(256) not null, player_ID varchar(256) not null, round_k int not null, \
     position int not null, player_chips varchar(256), action_id varchar(256))''')
 conn.commit()
