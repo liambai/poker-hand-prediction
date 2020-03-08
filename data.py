0779a524724e4af53f7320cce550e084498dc854
jlgood23 <jlgood23@icloud.com>
2020-03-08 17:34:40 -0400

    Update data.py

--- data.py
+++ data.py
@@ -12,34 +12,34 @@ c.execute('DROP TABLE IF EXISTS "Actions";')
 
 # Create tables in the database and add data to it.
 c.execute('''CREATE TABLE Games(
-    game_ID varchar(256) PRIMARY KEY, 
-    big_blind float, 
-    card1 varchar(256), 
-    card2 varchar(256), 
-    card3 varchar(256), 
-    card4 varchar(256), 
-    card5 varchar(256), 
-    pre_flop_pot float, 
-    post_flop_pot float, 
-    post_turn_pot float, 
+    game_ID varchar(256) PRIMARY KEY,
+    big_blind float,
+    card1 varchar(256),
+    card2 varchar(256),
+    card3 varchar(256),
+    card4 varchar(256),
+    card5 varchar(256),
+    pre_flop_pot float,
+    post_flop_pot float,
+    post_turn_pot float,
     post_river_pot float)''')
 
 c.execute('''CREATE TABLE Hands(
-    game_ID varchar(256) not null, 
-    player_ID varchar(256) not null, 
-    card_1 varchar(256), 
-    card_2 varchar(256), 
-    bets float, 
-    collects float, 
-    chips_at_begining float, 
+    game_ID varchar(256) FOREIGN KEY,
+    player_ID varchar(256) not null,
+    card_1 varchar(256),
+    card_2 varchar(256),
+    bets float,
+    net_gain float, 
+    chips_at_begining float,
     PRIMARY KEY(player_ID, game_ID))''')
 
 c.execute('''CREATE TABLE Actions(
-    game_ID varchar(256) not null, 
-    player_ID varchar(256) not null, 
-    round_k int not null, 
-    pos_in_round int not null, 
-    action_id varchar(256), 
+    game_ID varchar(256) FOREIGN KEY,
+    player_ID varchar(256) not null,
+    round_k int not null,
+    pos_in_round int not null,
+    action_id varchar(256),
     PRIMARY KEY(player_ID, game_ID, round_k, pos_in_round))''')
 
 conn.commit()
