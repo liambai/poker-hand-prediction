ee00183d19eee3fd8f429f8b6737ceed992339ac
Liam Bai <liambai@liams-mbp-2.devices.brown.edu>
2020-03-08 16:27:08 -0400

    first

--- data.py
+++ data.py
@@ -1,13 +1,6 @@
-from bs4 import BeautifulSoup
-import requests
 import sqlite3
-import urllib
 
-
-
-
-
-conn = sqlite3.connect(<database file>)
+conn = sqlite3.connect('poker.db')
 c = conn.cursor()
 
 # Delete tables if they exist
@@ -16,14 +9,16 @@ c.execute('DROP TABLE IF EXISTS "Hands";')
 c.execute('DROP TABLE IF EXISTS "Actions";')
 
 #Create tables in the database and add data to it. REMEMBER TO COMMIT
-c.execute('''CREATE TABLE Games(game_ID varchar(256) not null, cards varchar(256), pre_flop_pot float, post_flop_pot float, post_turn_pot float, post_river_pot float)''')
-c.execute('''CREATE TABLE Hands(game_ID varchar(256) not null, player_ID varchar(256) not null, card_1 varchar(256), card_2 varchar(256), bets float, collects float, chips_at_begining float)''')
+c.execute('''CREATE TABLE Games(game_ID varchar(256) not null, stakes varchar(256), cards varchar(256), pre_flop_pot float, \
+    post_flop_pot float, post_turn_pot float, post_river_pot float)''')
+c.execute('''CREATE TABLE Hands(game_ID varchar(256) not null, player_ID varchar(256) not null, card_1 varchar(256), \
+    card_2 varchar(256), bets float, collects float, chips_at_begining float)''')
 c.execute('''CREATE TABLE Actions(game_ID varchar(256) not null, player_ID varchar(256) not null, round_k int not null, \
     position int not null, player_chips varchar(256), action_id varchar(256))''')
 conn.commit()
 
-#use to add stuff:
-    c.execute('''INSERT INTO Games VALUES (?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
-    c.execute('''INSERT INTO Hands VALUES (?, ?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
-    c.execute('''INSERT INTO Actions VALUES (?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
-    conn.commit()
+# #use to add stuff:
+#     c.execute('''INSERT INTO Games VALUES (?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
+#     c.execute('''INSERT INTO Hands VALUES (?, ?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
+#     c.execute('''INSERT INTO Actions VALUES (?, ?, ?, ?, ?, ?)''', (<Add stuff in here>))
+#     conn.commit()
